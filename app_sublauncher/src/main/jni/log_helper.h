cd60d33a33119f730d72116c5e18ea25878fade7
Subhobhai <sarkarsubhadip604@gmail.com>
2025-11-08 21:24:40 +0530

    Add native logging header

---
+++ app_sublauncher/src/main/jni/log_helper.h
@@ -0,0 +1,13 @@
+#ifndef SUBLAUNCHER_LOG_H
+#define SUBLAUNCHER_LOG_H
+
+#include <android/log.h>
+
+#define LOG_TAG "SubLauncher-Native"
+
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+#endif // SUBLAUNCHER_LOG_H
