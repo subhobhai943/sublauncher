469bd213243b429000c200a07d7e48ab540954cd
Subhobhai <sarkarsubhadip604@gmail.com>
2025-11-08 22:24:16 +0530

    Update Android.mk to include jre_launcher.c

--- app_sublauncher/src/main/jni/Android.mk
+++ app_sublauncher/src/main/jni/Android.mk
@@ -8,21 +8,20 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := sublauncher
 LOCAL_CFLAGS := -Wall -Wextra -O2
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2 -ldl
 
 # Source files
 LOCAL_SRC_FILES := \
     sublauncher_bridge.c \
     utils.c \
-    log_helper.c
+    log_helper.c \
+    jre_launcher.c
 
 include $(BUILD_SHARED_LIBRARY)
 
-# Note: Full native components from PojavLauncher will be added later
-# These include:
+# Note: Additional native components can be added:
 # - awt_bridge.c (Java AWT to Android bridge)
 # - egl_bridge.c (OpenGL ES context management)
 # - input_bridge_v3.c (Keyboard/mouse input handling)
-# - jre_launcher.c (JRE initialization)
 # - GL4ES library (OpenGL to OpenGL ES translation)
 # - LWJGL3 native bindings
